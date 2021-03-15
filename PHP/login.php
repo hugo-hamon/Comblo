6b73924bc1c7cb052de574b3f29813214828aca3
Arkazix <hugohamon27@gmail.com>
2021-03-15 11:39:29 +0100

    Ajout html / CSS login

--- PHP/login.php
+++ PHP/login.php
@@ -3,9 +3,22 @@
 <head>
 	<meta charset="utf-8">
 	<title>Log in</title>
-	<link rel="stylesheet" type="text/css" href="../CSS/login.css">
+	<link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
-	<p>Test</p>
+	<div class="container">
+		<div class="login-form">
+			<form action="login_traite.php" method="post">
+				<div class="login">
+					<h3>Login</h3>
+				</div>
+					<input class="input" type="text" name="username" placeholder="Username">
+					<input class="input" type="password" name="password" placeholder="Password">
+					<input class="btn "type="submit" name="login" value="Log In">
+					<p>Pas encore inscris ? <a href="signup.php"> Nous rejoindres</a> </p>
+			</form>
+		</div>
+	</div>
+	<img src="../IMG/sunset.png">
 </body>
 </html>
