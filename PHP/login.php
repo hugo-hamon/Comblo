976ba6c5eb4925792c12973ceb5ffc01ff0decf5
Arkazix <hugohamon27@gmail.com>
2021-03-15 10:12:40 +0100

    Ajout fichier signup / log in

---
+++ PHP/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Log in</title>
+	<link rel="stylesheet" type="text/css" href="../CSS/login.css">
+</head>
+<body>
+	<p>Test</p>
+</body>
+</html>
