c4bdcdd356a202138264bcbdb321be6dd907d645
Arkazix <hugohamon27@gmail.com>
2021-03-22 18:20:42 +0100

    fixe page principale

--- PHP/signup_traite.php
+++ PHP/signup_traite.php
@@ -15,13 +15,11 @@
   $user_email = !empty($_POST['email']) ? $_POST['email'] : NULL;
   $user_pwd1 = !empty($_POST['password1']) ? $_POST['password1'] : NULL;
   $user_pwd2 = !empty($_POST['password2']) ? $_POST['password2'] : NULL;
-  $errors = array();
-
+  $errors = ["", "", "", ""];
+  
   //Test variable vide
   if ($user_pseudo == NULL or $user_email == NULL or $user_pwd1 == NULL or $user_pwd2 == NULL){
-  	array_push($errors, "Veuillez remplir les champs");
-  } else {
-    array_push($errors, "");
+  	$errors[0] = "Veuillez remplir les champs";
   }
 
   //Test adresse email valide
@@ -29,20 +27,16 @@
   $result = mysqli_query($conn, $query);
 
   if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)){
-    array_push($errors, "Email invalide");
+    $errors[1] = "Email invalide";
   }else if($result){
     if (mysqli_num_rows($result) >= 1){
-      array_push($errors, "Email déja pris");
+      $errors[1] = "Email déja pris";
     }
-  } else {
-    array_push($errors, "");
   }
   
   //Test mot de passe
   if ($user_pwd1 != $user_pwd2 or strlen($user_pwd1) < 4){
-    array_push($errors, "Mauvais mot de passes");
-  } else {
-    array_push($errors, "");
+    $errors[2] = "Mauvais mot de passes";
   }
 
   //Test pseudo déja existant
@@ -51,9 +45,7 @@
   
   if($result){
     if (mysqli_num_rows($result) >= 1){
-      array_push($errors, "Pseudo déja pris");
-    } else {
-      array_push($errors, "");
+      $errors[3] = "Pseudo déja pris";
     }
   }
 
